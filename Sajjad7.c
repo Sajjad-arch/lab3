5792ca23e44730e1737c6431369a94e1ff4d614c
Sajjad-arch <sajjadulislam870@gmail.com>
2024-12-10 12:58:42 +0600

    Add files via upload

---
+++ Sajjad7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int main(void)
+
+
+{
+
+    int gInt;
+    float gFloat;
+
+
+    scanf("%f", &gFloat); //Input 46.87
+
+
+    gInt = (int) gFloat;
+
+    printf("The given floating number is: %f\n", gFloat);
+    printf("The given floating number (more accurately) is: %2f\n", gFloat);
+
+    print("The integer representation will be: %d\n", gInt);
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
